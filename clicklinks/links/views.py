4528dd4102b120411631580e2144d490643f499e
miskhodzh <miskhodzhev.osman@yandex.ru>
2023-09-15 06:01:27 +0300

    отображение страниц пользователей

--- clicklinks/links/views.py
+++ clicklinks/links/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
+
+from .models import Links
+
+User = get_user_model()
 
 
 def links(request, username):
     template = 'links/main-template.html'
+    user = get_object_or_404(User, username=username)
+    links = Links.objects.filter(user_id=user)
     context = {
-        'username': username,
+        'user': user,
+        'links': links,
     }
     return render(request, template, context)
