9a2d994ebde1d7013833d0404b8952cf4f35f02c
miskhodzh <miskhodzhev.osman@yandex.ru>
2023-11-26 21:25:54 +0300

    начал перебирать статику

--- clicklinks/clicklinks/settings.py
+++ clicklinks/clicklinks/settings.py
@@ -107,10 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = 'static/'
+STATIC_ROOT='static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static_dev'
+    BASE_DIR / 'static/'
 ]
 
 # Default primary key field type
