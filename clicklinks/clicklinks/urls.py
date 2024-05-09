b658b283a6588d6fab0a13af29cc6e4339ad57da
miskhodzh <miskhodzhev.osman@yandex.ru>
2024-05-09 09:20:17 +0300

    clean

--- clicklinks/clicklinks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('auth/', include('users.urls'), name='users'),
-    path('admin/', admin.site.urls),
-]
--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, '/var/www/u2240405/data/www/clicklinks.ru/clicklinks')
-sys.path.insert(1, '/var/www/u2240405/data/venv/lib/python3.10/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'clicklinks.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
